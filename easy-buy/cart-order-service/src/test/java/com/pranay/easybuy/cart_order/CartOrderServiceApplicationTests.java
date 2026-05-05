a336a5bd77c818160ced24e0cecf8cd8f04fe73f
pranaybhoir <pranaytb777@gmail.com>
2026-05-05 11:55:59 +0530

    Initialize microservices architecture with Spring Boot projects for AI, Cart-Order, Inventory, Notification, Payment, Products, and User services. Include Maven wrapper, set up `.gitattributes`, and configure application properties.

---
+++ easy-buy/cart-order-service/src/test/java/com/pranay/easybuy/cart_order/CartOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranay.easybuy.cart_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

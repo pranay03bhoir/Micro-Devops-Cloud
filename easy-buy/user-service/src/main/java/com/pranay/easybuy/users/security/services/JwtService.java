5dc3dfb8b52d22c5db633b6a8bf165622a0e4686
pranaybhoir <pranaytb777@gmail.com>
2026-06-29 18:10:45 +0530

    feat: add token-based authentication filter in API Gateway
    - Implemented `AuthenticationFilter` to verify and enforce JWT-based authentication and role-based access control in API Gateway.
    - Introduced JWT secret configuration in `application.yaml`.
    - Enabled public and admin-specific endpoint support with user/role validation and resource ownership checks in `AuthenticationFilter`.
    - Integrated the authentication filter into route configurations in `RouteConfig`.
    - Enhanced `JwtService` in the user service to enable consistent token parsing across services.
    - Updated IDE settings to remove `activate` option in `.idea/saveactions_settings.xml`.

--- easy-buy/user-service/src/main/java/com/pranay/easybuy/users/security/services/JwtService.java
+++ easy-buy/user-service/src/main/java/com/pranay/easybuy/users/security/services/JwtService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
@@ -32,7 +33,7 @@ public class JwtService {
     public JwtService(@Value("${jwt.secret}") String key,
                       @Value("${jwt.access-token-expiration}") long accessTokenExpiration,
                       @Value("${jwt.refresh-token-expiration}") long refreshTokenExpiration) {
-        this.secretKey = Keys.hmacShaKeyFor(key.getBytes());
+        this.secretKey = Keys.hmacShaKeyFor(key.getBytes(StandardCharsets.UTF_8));
         this.accessTokenExpiration = accessTokenExpiration;
         this.refreshTokenExpiration = refreshTokenExpiration;
     }
